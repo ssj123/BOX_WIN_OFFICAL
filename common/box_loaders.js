2c99e1c3c658df8c65e145ef9f9e8ed3dc83ff1c
shengjing song <ssj0817@163.com>
2017-12-26 13:12:45 +0800

    修复排版bug

--- common/box_loaders.js
+++ common/box_loaders.js
@@ -1,8 +1,8 @@
 document.write("<script type='text/javascript' src='http://cdn.bootcss.com/zui/1.7.0/lib/jquery/jquery.js?v=" + _global.h5_version + "'><\/script>");
 
-document.write("<script type='text/javascript' src='/BOX_WIN_OFFICAL/lib/zui/zui.js?v=" + _global.h5_version + "'><\/script>");
+document.write("<script type='text/javascript' src='./lib/zui/zui.js?v=" + _global.h5_version + "'><\/script>");
 
-document.write("<script type='text/javascript' src='/BOX_WIN_OFFICAL/common/common.js?v=" + _global.h5_version + "'><\/script>");
+document.write("<script type='text/javascript' src='./common/common.js?v=" + _global.h5_version + "'><\/script>");
 
-document.write("<script type='text/javascript' src='/BOX_WIN_OFFICAL/business/business.js?v=" + _global.h5_version + "'><\/script>");
+document.write("<script type='text/javascript' src='./business/business.js?v=" + _global.h5_version + "'><\/script>");
 
